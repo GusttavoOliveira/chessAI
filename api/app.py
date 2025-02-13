fbbf7fc9ac6eff2a1d3126e01b69557fd54cd96f
Luiz Gusttavo <luiz.gusttavo@academico.ufpb.br>
2025-02-13 19:22:52 -0300

    bugs fixes and try to run

--- api/app.py
+++ api/app.py
@@ -1,6 +1,12 @@
 from flask import Flask, render_template, jsonify, request, send_file
 from flask_socketio import SocketIO
+import sys
+import os
+
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
 from scripts.chess_logic import ChessGame
+
 
 app = Flask(__name__)
 socketio = SocketIO(app, cors_allowed_origins="*")
